80f752991d4b4c63f5187048b9dbfb0a500c48b0
Dorukante <72575981+Dorukante@users.noreply.github.com>
2021-05-30 16:18:15 +0200

    Update WebPage.py

--- WebPage.py
+++ WebPage.py
@@ -1,8 +1,11 @@
 from os import link
 from flask import Flask, app, render_template, request, jsonify
 import pandas as pd
-import csv,json
+import csv
 import os
+from Classes.ForceDirectedFormat import ForceDirectedFormat
+
+from pandas.core.base import DataError
 
 
 
@@ -10,11 +13,13 @@ import os
 app =Flask(__name__,template_folder= "Template")
 
 
-#Bunu silme
+#Root for the main page
 @app.route('/', methods = ['GET', 'POST'])
 def homepage():
    return render_template("Base.html")
 
+
+#Root for the datasets page
 @app.route('/data')
 def data():
     filename = './static/enron-v1.csv'
@@ -26,40 +31,48 @@ def data():
 
    
 
-#Bunu silme
+#Root for the visualizations page
 @app.route('/visualizations/', methods = ['GET', 'POST'])
 def visulizations():
     if request.method == 'POST':
       file = request.files['csvfile']
       if not os.path.isdir('static'):
          os.mkdir('static')
-      filepath = os.path.join('Template', os.path.splitext(file.filename)[0] + '.json')
-      file.save(filepath)
-      data_new = {}
-      with open(filepath) as csvFile_new:
+
+      csv_filepath = os.path.join('static', file.filename)
+      json_filepath = os.path.join('Template', os.path.splitext(file.filename)[0] + '.json')
+      file.save(csv_filepath)
+      
+      data = {}
+
+      with open(csv_filepath) as csvFile_new:
           csvReader_new = csv.DictReader(csvFile_new)
           for csvRow_new in csvReader_new:
               fromEmail = csvRow_new['fromEmail']
-              data_new[fromEmail] = csvRow_new
+              if (fromEmail not in data):
+                 data[fromEmail] = [csvRow_new]
+              else:
+                 data[fromEmail].append(csvRow_new)
 
-      with open(filepath, "w") as jsonFile:
-         Json_any_file = jsonFile.write(json.dumps( data_new, indent=6))
-         return "The uploaded file is: " + file.filename + render_template("AQ.html")
+      fd_format = ForceDirectedFormat()
+      fd_format.writeToFile(data)
+     
+      return  render_template("AQ.html")
     
-    return render_template("AQ.html")
+    return render_template("AQ.html", message = "Pleas upload a csv file that is related to the Enron")
 
 
-
+#Root for the json files
 @app.route('/visualizations/<filename>.csv', methods = ['GET', 'POST'])
 def Enron_anyfile(filename):
    #check if the file exists
 
-   with open('./Template/' + filename + '.json', 'r') as file:
+   with open('C:\\Users\\20201077\\Desktop\\Doruk Güngör\\Eindhoven\\Computer Science\\Year 1\\Q4\\DBL-Webtech\\Phyton\\Template/' + filename + '.json', 'r') as file:
       Json_any_file = file.read()
       return Json_any_file
 
 
 
-#Bunu silme
+#This is important
 if __name__ == "__main__":
     app.run(debug=True)
