3c899761f1eac0f2123e761e63015003e91eb6ae
Dorukante <72575981+Dorukante@users.noreply.github.com>
2021-06-10 17:36:12 +0200

    Update ForceDirectedFormat.py

--- Classes/ForceDirectedFormat.py
+++ Classes/ForceDirectedFormat.py
@@ -10,12 +10,20 @@ class ForceDirectedFormat(JsonFormat):
         data_fd_graph = self.schema
 
         for key in data:
-            data_fd_graph['nodes'].append({'id': key, 'group': data[key][0]["fromJobtitle"]})
+            data_fd_graph['nodes'].append({'id': key, 'group': data[key][0]["fromJobtitle"]})#, 'fromId': data[key][0]['fromId']})
                 
             for item in data[key]:
-                data_fd_graph['links'].append({'source': item['fromEmail'], 'target': item['toEmail'], 'sentiment': item['sentiment']})
-                if ({'id': item['toEmail'], 'group': item['toJobtitle']} not in data_fd_graph['nodes']):
-                    data_fd_graph['nodes'].append({'id': item['toEmail'], 'group': item["toJobtitle"]})
+                to_append = {'source': item['fromEmail'], 'target': item['toEmail']}
+
+                if to_append not in [{i:data_item[i] for i in data_item if i != 'email_count'} for data_item in data_fd_graph['links']]:
+                    data_fd_graph['links'].append(to_append)
+                    data_fd_graph['links'][data_fd_graph['links'].index(to_append)]['email_count'] = len([link_item for link_item in data_fd_graph['links'] if {i: link_item[i] for i in link_item if i != 'emails_count'} == to_append])
+                
+                to_append = {'id': item['toEmail'], 'group': item['toJobtitle']}#, 'fromId': item['toId']}
+
+                if (to_append not in data_fd_graph['nodes']):
+                    data_fd_graph['nodes'].append(to_append)
+                
 
         with open(self.filepath, "w") as jsonFile:
             jsonFile.write(json.dumps(data_fd_graph, indent=6))
