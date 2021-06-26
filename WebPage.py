51feb7945bd3fc0ad64dabee2f10837409b5104b
Dorukante <72575981+Dorukante@users.noreply.github.com>
2021-06-26 14:16:02 +0200

    Update WebPage.py

--- WebPage.py
+++ WebPage.py
@@ -1,71 +1,84 @@
 from os import link
 from flask import Flask, app, render_template, request, jsonify
 import pandas as pd
-import csv, os
+import csv
+import os
 from Classes.ForceDirectedFormat import ForceDirectedFormat
 from Classes.Heatmap import Heatmap
 from Classes.PieChart import PieChart
+import time
+
+# Bunu silme
+app = Flask(__name__, template_folder="Template")
+
+# Root for the main page
 
 
-
-
-#Bunu silme
-app =Flask(__name__,template_folder= "Template")
-
-
-#Root for the main page
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
-   return render_template("Base.html")
+    return render_template("Base.html")
 
 
-#Root for the visualizations page
-@app.route('/visualizations/', methods = ['GET', 'POST'])
+# Root for the visualizations page
+@app.route('/visualizations/', methods=['GET', 'POST'])
 def visulizations():
     if request.method == 'POST':
-      file = request.files['csvfile']
-      if not os.path.isdir('static'):
-         os.mkdir('static')
+        file = request.files['csvfile']
+        if not os.path.isdir('static'):
+            os.mkdir('static')
 
-      csv_filepath = os.path.join('static', file.filename)
-      json_filepath = os.path.join('Template', os.path.splitext(file.filename)[0] + '.json')
-      file.save(csv_filepath)
-      
-      data = {}
+        csv_filepath = os.path.join('static', file.filename)
+        json_filepath = os.path.join(
+            'Template', os.path.splitext(file.filename)[0] + '.json')
+        file.save(csv_filepath)
 
-      with open(csv_filepath) as csvFile_new:
-          csvReader_new = csv.DictReader(csvFile_new)
-          for csvRow_new in csvReader_new:
-              fromEmail = csvRow_new['fromEmail']
-              if (fromEmail not in data):
-                 data[fromEmail] = [csvRow_new]
-              else:
-                 data[fromEmail].append(csvRow_new)
+        data = {}
 
-      fd_format = ForceDirectedFormat()
-      fd_format.writeToFile(data)
+        with open(csv_filepath) as csvFile_new:
+            csvReader_new = csv.DictReader(csvFile_new)
+            for csvRow_new in csvReader_new:
+                fromEmail = csvRow_new['fromEmail']
+                if (fromEmail not in data):
+                    data[fromEmail] = [csvRow_new]
+                else:
+                    data[fromEmail].append(csvRow_new)
 
-      heatmap_format = Heatmap()
-      heatmap_format.writeToFile(data)
+        start = time.time()
 
-      PieChart_format = PieChart()
-      PieChart_format.writeToFile(data)
-     
-     
-      return  render_template("AQ.html")
-    
-    return render_template("AQ.html", message = "Please upload a csv file that is related to the Enron")
+        fd_format = ForceDirectedFormat()
+        fd_format.writeToFile(data)
+
+        heatmap_format = Heatmap()
+        heatmap_format.writeToFile(data)
+
+        PieChart_format = PieChart()
+        PieChart_format.writeToFile(data)
+
+        end = time.time()
+
+        print("Time elapsed: %.6f" % (end - start))
+
+        return render_template("AQ.html")
+
+    return render_template("AQ.html")
 
 
-#Root for the json files
-@app.route('/visualizations/<filename>', methods = ['GET', 'POST'])
+# Root for the json files
+@app.route('/visualizations/<filename>', methods=['GET', 'POST'])
 def Enron_anyfile(filename):
-   #check if the file exists
+    # check if the file exists
 
-   with open('./Template/' + filename + '.json', 'r') as file:
-      Json_any_file = file.read()
-      return Json_any_file
+    with open('./Template/' + filename + '.json', 'r') as file:
+        Json_any_file = file.read()
+        return Json_any_file
 
+
+@app.route('/get-<filename>', methods=['GET', 'POST'])
+def getResource(filename):
+    # check if the file exists
+
+    with open('./Template/' + filename, 'r') as file:
+        return file.read()
 
 
 #This is important
